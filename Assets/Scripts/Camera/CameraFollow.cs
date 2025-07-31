d94c9fbe2c96143a12500ddc39a01e4e263539ad
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-31 18:31:07 -0500

    Add project files.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform PlayerPosition;
+    public float smoothing = 5f;
+
+    private Vector3 offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = transform.position - PlayerPosition.position;
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector3 playerCamPosition = PlayerPosition.position + offset;
+        transform.position = Vector3.Lerp(transform.position, playerCamPosition, smoothing * Time.deltaTime);
+    }
+}
